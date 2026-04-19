6fd946c3bb126e165d09bcf42cb1ed6df7688a40
ruban0122 <yogaruban2201@gmail.com>
2026-04-19 14:14:42 +0800

    Improved SEO

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'brainypulse.com' }],
+        destination: 'https://www.brainypulse.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.brainypulse.com' }],
+        missing: [{ type: 'header', key: 'x-forwarded-proto', value: 'https' }],
+        destination: 'https://www.brainypulse.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
